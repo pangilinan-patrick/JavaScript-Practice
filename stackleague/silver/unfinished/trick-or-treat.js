3b0c3966b17619dd8996b546dc2294c3320036cb
pangilinan-patrick <97028950+pangilinan-patrick@users.noreply.github.com>
2023-03-14 21:32:41 +0800

    trick or treat problem

--- stackleague/silver/unfinished/trick-or-treat.js
+++ stackleague/silver/unfinished/trick-or-treat.js
@@ -1,12 +1,62 @@
+// Unfinished
 function trick_or_treat(candies, threshold) {
-  let results = [34, 24, 25];
+  let accTotal = [];
+  let result = [];
+
   for (let i = 0; i < candies.length; i++) {
-    results[i] = 0;
+    let total = 0;
     for (let j = 0; j < candies.length; j++) {
-      results += candies[j];
+      // initial check if candy is under threshold
+      if (candies[i] + candies[j] < threshold && i !== j) {
+        // check if total still hasn't broken threshold after adding two numbers
+        if (total > 0 && (total += candies[j]) < threshold) {
+          accTotal[i].unshift(j);
+          total += candies[j];
+          accTotal[i][accTotal[i].length - 1] += candies[j];
+          console.log("if");
+        } else if (total === 0) {
+          // if total hasn't yet been added
+          total = candies[i] + candies[j];
+          console.log("else");
+        } else break;
+        accTotal.push([i, j, total]);
+        console.log(i, j, candies[i], candies[j], total);
+        console.log(accTotal);
+      }
     }
   }
-  console.log(results);
+  // return empty array if no satisfactory result
+  if (accTotal.length === 0) return [];
+
+  // for (let i = 0; i < accTotal.length; i++) {
+  //   accTotal[i].sort((a, b) => a - b);
+  // }
+  // accTotal.sort((a, b) => a[0] - b[0]);
+
+  let minValue = accTotal[0][accTotal[0].length - 1];
+  for (let i = 0; i < accTotal.length; i++) {
+    if (accTotal[i][accTotal[i].length - 1] < minValue) {
+      minValue = accTotal[i][accTotal[i].length - 1];
+    }
+  }
+
+  console.log(accTotal);
+  console.log(minValue);
+
+  for (let i = 0; i < accTotal.length; i++) {
+    if (accTotal[i][accTotal[i].length - 1] === minValue)
+      console.log(accTotal[i]);
+  }
+  return result;
 }
 
-trick_or_treat([33, 20, 12, 19, 29], 33);
+// let candies = [72, 6, 84, 9, 10];
+// let threshold = 99;
+
+// let candies = [62, 67, 100];
+// let threshold = 8;
+
+let candies = [6, 88, 1, 12, 15];
+let threshold = 33;
+
+trick_or_treat(candies, threshold);
